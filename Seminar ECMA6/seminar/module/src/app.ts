67ae772dcfd6c9727abef11996c1049fd169ba00
grisham88 <grisham88@gmx.net>
2018-12-14 09:01:33 +0100

    Modules

--- Seminar ECMA6/seminar/module/src/app.ts
+++ Seminar ECMA6/seminar/module/src/app.ts
@@ -8,26 +8,23 @@ import { value2 as value2Modul1 } from './modul1';
 // ICH WILL ALLES! Namespacing mit Aliasobjekt
 import * as tools from './modul2';
 
-//Defaultimport
+import { superTools } from './modul3';
+// Defaultimport
 import toolset from './modul3';
 
 // Import aus Verzeichnis (Barrel):
-import { myTool1 } from './tools';
-// spezielle Sachen die nicht im Barrel sind
+import { myTool1 } from './tools'
+// spezielle Sachen, die nicht im Barrel sind:
 import { myTool1Variant } from './tools/tool1'
 
 console.log('Der Sinn des Lebens:', value1);
 let value2 = 'Ich bin schon belegt!'; // !
 console.log('Alias:', value2Modul1);
 
-let propA = toolObject.a;
+let propA = toolObject.a; 
 console.log('prop a aus toolObject', propA);
 
 // Pauschalimport
 console.log(tools);
-//Defaultimport
+// Defaultimport
 console.log(toolset);
-
-//Verzeichnisimport
-console.log(myTool1);
-console.log(myTool1Variant);
--- Seminar ECMA6/seminar/module/src/tools/index.ts
+++ Seminar ECMA6/seminar/module/src/tools/index.ts
@@ -1,6 +1,12 @@
 import { myTool1 } from './tool1';
 import { myTool2 } from './tool2';
 
+let megaTool = {
+    myTool1, myTool2
+};
+
 export { myTool3 } from './tool3';
 
 export { myTool1, myTool2 }
+
+export default megaTool;
